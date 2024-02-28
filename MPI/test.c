111d8f0fd3aca27c5fc0e51c3d74daa9a6b2806e
Ginger Heef <gingerheef003@gmail.com>
2024-02-28 14:26:22 +0530

    mpi for loop

--- MPI/test.c
+++ MPI/test.c
@@ -1,8 +1,11 @@
+#include <math.h>
 #include <stdio.h>
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-	int rank, n_ranks;
+	int rank, n_ranks, numbers_per_rank;
+	int my_first, my_last;
+	int numbers = 10;
 
 	
 	MPI_Init(&argc, &argv);
@@ -10,8 +13,19 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
 
-	printf("My rank number is %d\n", rank);
-	printf("Total no. of ranks = %d\n", n_ranks);
-	
+	numbers_per_rank = floor(numbers/n_ranks);
+	if(numbers%n_ranks > 0) {
+		numbers_per_rank += 1;
+	}
+
+	my_first = rank * numbers_per_rank;
+	my_last = my_first + numbers_per_rank;
+
+	for(int i = my_first; i < my_last; i++) {
+		if(i < numbers) {
+			printf("I'm rank %d and I'm printing the number %d. \n", rank, i);
+		}
+	}
+
 	return MPI_Finalize();
 }
