0a10ae82ec741b52c89a7a3a8b900791e9211153
Ginger Heef <gingerheef003@gmail.com>
2024-02-28 14:48:06 +0530

    mpi send recv to master

--- MPI/send_recv.c
+++ MPI/send_recv.c
@@ -10,25 +10,19 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	if(rank%2 == 0) {
-		my_pair = rank + 1;
+	if(rank != 0) {
+		char message[30];
+		sprintf(message, "Hello world, I'm rank %d\n", rank);
+		MPI_Send(message, 30, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 	} else {
-		my_pair = rank - 1;
-	}
-
-	if(my_pair < n_ranks) {
-		if(rank%2 == 0) {
-			char *message = "Hello, world!\n";
-			MPI_Send(message, 16, MPI_CHAR, my_pair, 0, MPI_COMM_WORLD);
-		} else {
+		for(int sender = 1; sender < n_ranks; sender++) {
 			char message[16];
 			MPI_Status status;
-			MPI_Recv(message, 16, MPI_CHAR, my_pair, 0, MPI_COMM_WORLD, &status);
+			
+			MPI_Recv(message, 30, MPI_CHAR, sender, 0, MPI_COMM_WORLD, &status);
 			printf("%s", message);
 		}
-
-
 	}
-
-		return MPI_Finalize();
+	
+	return MPI_Finalize();
 }
